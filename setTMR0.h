91989b7f8748cc6dbf8ff2a9cc1211d74114b148
IlijaVorontsov <32547475+IlijaVorontsov@users.noreply.github.com>
2017-10-15 17:39:22 +0200

    Tidied up code.

--- setTMR0.h
+++ setTMR0.h
@@ -1,43 +1,52 @@
 /*
- * File:   setTMR0.h
- * Author: Ilija Vorontsov  
- * Programm zur automatischen dimensionierung des Prescalers, sowie setzen der
- * TMR0L und TMR0H SFR.
- * Created on 16. Mai 2017, 15:25
+  File:         setTMR0.h
+  Author:       Ilija Vorontsov
+  Discription:  Sets the Prescaler automaticly and fills Timer0 counter registers according
+                to the given time-value.
+
+  Created on 16. May 2017, 15:25
  */
 
-long setPreScaler(long t);
-void setTMR0_ms(long t);
+
+// Calculate for your own Oszillator:   TimeConst = FreqOsz / 4000
+#define TimeConst 2765
 
 
-void setTMR0_ms(long t) {
-    long PreScaler=0, StartingValue=0;              //Variablen initialisiert und deklariert
-    
-    PreScaler = setPreScaler(t);           //Prescaler einstellen
+long setPreScaler(long timeval);
+void setTMR0_ms(long timeval);
 
-    StartingValue=0x10000 - t*2765/PreScaler;    //Berechnung der Einstellung für Timer0
-    TMR0L=StartingValue%256;    //Timer 0 Low Byte setzen
-    TMR0H=StartingValue/256;    //Timer 0 High Byte setzen
-    
-}   //Programm zum setzen des Timer0 Low und High bytes
+
+void setTMR0_ms(long timeval) {
+  long PreScaler = 0 , StartingValue = 0;
+
+  // Calculating and setting the prescaler.
+  PreScaler = setPreScaler(timeval);
+
+  // Calculating and setting startingvalues for both Timer 0 registers.
+  StartingValue = 0x10000 - timeval * TimeConst / PreScaler;
+  TMR0L = StartingValue % 256;
+  TMR0H = StartingValue / 256;
+}
 
 long setPreScaler(long t){
- long PreScaler=0;
- char HighBitCounter=0;
- 
- PreScaler=(t*2765)/0x10000;     //Berechnen des idealen Prescalers  
- 
- while(PreScaler>1){            //Ermittlung der Stelle des höchseten Bits
-    HighBitCounter++;    
-    PreScaler=PreScaler>>1;      
- }
-    
-    PreScaler = PreScaler<<(HighBitCounter+1);           //Einstellen des Bits um eine Stelle höher
-    if(PreScaler>1){               
-        T0CON=HighBitCounter; //Prescaler einstellen
-    }else{
-        T0CON=8; //PSA High rest Low
-    }
-    return(PreScaler);
-}   //Programm zur Berechnung und setzen des Prescalers
+ long PreScaler = 0;
+ char HighBitCounter = 0;
 
+ // Calculating ideal prescaler.
+ PreScaler = (t * TimeConst) / 0x10000;
+
+ //Calculating real prescaler.
+ while(PreScaler > 1){
+    HighBitCounter++;
+    PreScaler = PreScaler >> 1;
+  }
+  PreScaler = PreScaler << (HighBitCounter + 1);
+
+  //Setting prescaler
+  if(PreScaler > 1){
+    T0CON = HighBitCounter;
+  }else{
+      T0CON = 8;
+    }
+  return(PreScaler);
+  }
